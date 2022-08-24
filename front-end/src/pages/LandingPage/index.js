6738fc8661dc19190fa29a69bc9902fb4f0a7aad
matheus.venera <matheusogliarivenera@gmail.com>
2022-08-24 11:44:48 -0300

    Feito estilo inicial da página inicial

--- front-end/src/pages/LandingPage/index.js
+++ front-end/src/pages/LandingPage/index.js
@@ -1,13 +1,19 @@
 import React from "react";
-
+import './index.css'
 import { Container } from "../../styles/GlobalStyles";
 
 export default function LandingPage() {
   return (
     <>
-      <Container>
-        <h1>Landing page</h1>
-      </Container>
+      <img src={require('./illustration.jpg')} alt="School teacher" className="illustration"></img>
+      <p className="credits">
+      <a href='https://www.freepik.com/vectors/school-teacher'>School teacher vector created by storyset - www.freepik.com</a>
+      </p>
+      <section className="information">
+        <p className="text"><span className="title">Simplicidade</span> para gerenciar seus alunos, é disso que você precisa! E é aqui que você encontra.
+        
+        </p>
+      </section>
     </>
   );
 }
